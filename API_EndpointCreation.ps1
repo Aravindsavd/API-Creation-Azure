fd6165356fa5e3022521d4b02de8506b97d6e8c1
Aravind S <as5@aptean.com>
2026-05-20 19:53:02 +0530

    secound commit

--- API_EndpointCreation.ps1
+++ API_EndpointCreation.ps1
@@ -1,23 +1,23 @@
 #MohanKumarKannan - 19/03/2025 - This script will automate Paragon API Endpoint creation on Azure Application Gateway and creates Record set on DNS Zone
 #Applicable for all Region within Paragon AGW's
 Connect-AzAccount
-Set-AzContext "a-prod-002"
+Set-AzContext "Domain name" #change manually
 $clientname = Read-Host "Enter the Client Name"
 $region = Read-Host "Enter the Region (eastus2 or westuk or anze - only)"
 $environment = Read-Host "Enter the Environment (prd or uat)"
 $backendpoolmachineip = Read-Host "Enter the Backend VM Private IP"
 $listnerhostname = "$clientname-api.paragon.apteancloud.com"
-$backendpoolname = "beap-$region-$environment-pgn-$clientname-api"
-$listnername = "lst-$region-$environment-pgn-$clientname-api"
-$backendsettingsname = "htst-$region-$environment-pgn-$clientname-api"
-$healthprobename = "prb-$region-$environment-pgn-$clientname-api"
-$rulename = "fwr-$region-$environment-pgn-$clientname-api"
-$applicationgatewayname = "agw-$region-prd-pgn"
-$resourcegroupname = "rg-$region-prd-pgn"
+$backendpoolname = "beap-$region-$environment-$clientname-api"
+$listnername = "lst-$region-$environment-$clientname-api"
+$backendsettingsname = "htst-$region-$environment-$clientname-api"
+$healthprobename = "prb-$region-$environment-$clientname-api"
+$rulename = "fwr-$region-$environment--$clientname-api"
+$applicationgatewayname = "agw-$region-prd"
+$resourcegroupname = "rg-$region-prd"
 $dnsrecordname = "$clientname-api"
 
 $appgateway = Get-AzApplicationGateway -Name $applicationgatewayname -ResourceGroupName $resourcegroupname
-$sslCert = Get-AzApplicationGatewaySslCertificate -ApplicationGateway $appgateway -Name "wildcard.paragon-apteancloud.com"
+$sslCert = Get-AzApplicationGatewaySslCertificate -ApplicationGateway $appgateway -Name "wildcard.cloud.com" #choose certificate
 $frontendipconfig = Get-AzApplicationGatewayFrontendIPConfig -Name "appGwPublicFrontendIp" -ApplicationGateway $appgateway
 $frontendportconfig = Get-AzApplicationGatewayFrontendPort -ApplicationGateway $appgateway -Name "port_443"
 
@@ -81,12 +81,11 @@ Set-AzApplicationGateway -ApplicationGateway $appgateway
 
 #Create a DNS Record Set in the paragon.apteancloud.com Zone
 $Records = New-AzDnsRecordConfig -Cname "$dnsrecordname.paragon.apteancloud.com.cdn.cloudflare.net"
-$RecordSet = New-AzDnsRecordSet -Name $dnsrecordname -RecordType CNAME -ResourceGroupName "rg-westuk-prd-pgn"  -TTL 3600 -ZoneName "paragon.apteancloud.com" -DnsRecords $Records
+$RecordSet = New-AzDnsRecordSet -Name $dnsrecordname -RecordType CNAME -ResourceGroupName "rg-westuk-prd"  -TTL 3600 -ZoneName "paragon.apteancloud.com" -DnsRecords $Records
 
 Write-Host "Application Gateway is now updated for $clientname and New DNS Record Created as below"
 Write-Host $RecordSet.Records
-Write-Host "Create a SD Ticket for Cloudflare A Record"
-Write-Host "Client URL will be https://$dnsrecordname.paragon.apteancloud.com"
+Write-Host "Client URL will be https://$dnsrecordname.cloud.com" #change with correct dns name
              
 
 
